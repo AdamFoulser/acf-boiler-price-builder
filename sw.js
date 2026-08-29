f0c99a306da0bbdf56d947a58108f518367bfa17
AdamFoulser <adam_foulser@hotmail.co.uk>
2026-08-29 20:13:03 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='acf-boiler-v23';
-const ASSETS=['./','./index.html','./styles.css?v=22','./app.js?v=22','./data.js?v=22','./manifest.webmanifest','./IMG_9229.jpeg'];
+const CACHE='acf-boiler-v24';
+const ASSETS=['./','./index.html','./styles.css?v=24','./app.js?v=24','./data.js?v=24','./manifest.webmanifest','./IMG_9229.jpeg'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
