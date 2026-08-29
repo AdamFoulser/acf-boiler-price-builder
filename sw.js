e339543d787e28b9aa369c508912b5abebdebcb3
AdamFoulser <adam_foulser@hotmail.co.uk>
2026-08-29 21:37:33 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='acf-boiler-v28';
-const ASSETS=['./','./index.html','./styles.css?v=28','./app.js?v=28','./data.js?v=28','./manifest.webmanifest','./acf-boiler-pricing-logo.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
+const CACHE='acf-boiler-v29';
+const ASSETS=['./','./index.html','./styles.css?v=29','./app.js?v=29','./data.js?v=29','./manifest.webmanifest','./acf-boiler-pricing-logo.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
